684f0030e787dfd5c5214d9582adb05804f1ecaa
omengye <ym2008love@gmail.com>
2015-03-01 17:28:49 +0800

    add login page and jquery post

--- application/handlers/auth.py
+++ application/handlers/auth.py
@@ -25,6 +25,23 @@ class CheckPageHandler(Auth):
         self.render("ws.html", code=self.generate_code(), pic_name=self.gen_pic_name())
 
 
+class CheckLoginHandler(tornado.web.RequestHandler):
+    def get(self, par):
+        par = str(par)
+        if par is not None and Utils.no_special_symbol(par) and len(par) == 32:
+            self.render("check.html", code=par)
+        else:
+            self.redirect("http://127.0.0.1:8000/")
+
+    def post(self, par):
+        print(par)
+        email = self.get_argument("email", None)
+        passwd = self.get_argument("md5_pass", None)
+        print(email, passwd)
+        self.finish("200")
+
+
+
 class WebSocketHandler(Auth):
     def check_origin(self, origin):
         return True
@@ -37,7 +54,7 @@ class WebSocketHandler(Auth):
             box_size=6,
             border=1,
         )
-        qr.add_data(self.generate_code())
+        qr.add_data("http://192.168.1.106:8000/check/" + self.generate_code())
         qr.make(fit=True)
         img = qr.make_image()
         pic_path = os.path.join(os.path.dirname(__file__), "../../static/qrpic/") + self.gen_pic_name()
@@ -48,7 +65,6 @@ class WebSocketHandler(Auth):
         if len(message) == 32:
             self.write_message(message)
             print("send message")
-            self.close()
 
     def on_close(self):
         self.close()
--- application/urls.py
+++ application/urls.py
@@ -4,5 +4,6 @@ from application.handlers import base, auth
 urls = [
     (r"/", base.HomeHandler),
     (r"/ws", auth.CheckPageHandler),
-    (r"/socket", auth.WebSocketHandler)
+    (r"/socket", auth.WebSocketHandler),
+    (r"/check/([0-9a-z]+)", auth.CheckLoginHandler)
 ]
